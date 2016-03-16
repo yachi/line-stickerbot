1d9763b41f2b55f0e46653b79c5c73677be78a6e
alexskc <alexskc@autistici.org>
2016-03-16 15:06:35 -0500

    Return a zipped website on request.

--- imagedl.sh
+++ imagedl.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 URL=$1
-echo $URL
-curl -F chat_id="193224174" -F document=@$URL https://api.telegram.org/bot216513681:AAE38YYorq8ox8k1Pnqprn862Ut31SPq3So/sendDocument
+cd downloads
+wget -q -x $URL
+cd ${URL#"http://"}
+zip -r website.zip ./*
+curl -F chat_id="193224174" -F document=@"website.zip" https://api.telegram.org/bot216513681:AAE38YYorq8ox8k1Pnqprn862Ut31SPq3So/sendDocument
