bb80e684fa3a0cb1c00403c1283566cbc64c2420
alexskc <alexskc@autistici.org>
2016-03-17 06:44:24 -0500

    Fix #1

--- imagedl.sh
+++ imagedl.sh
@@ -4,9 +4,9 @@ CHAT=$2
 cd downloads
 wget -q -x -p --convert-links $URL
 FIRST=${URL#"https://"}
-SECOND=${FIRST/stickershop\/product\/*\/en/''}
+SECOND=${FIRST/stickershop\/product\/*\/*/''}
 cd $SECOND
-grep 'width: [0-9]*px; height: [0-9]*px; background-image:url(' ./stickershop/product/*/en > stickerlines
+grep 'width: [0-9]*px; height: [0-9]*px; background-image:url(' ./stickershop/product/*/* > stickerlines
 sed 's/                          <span class=".*" style="width: [0-9]*px; height: [0-9]*px; background-image:url(//g' stickerlines >> newstickers
 sed 's/); background-size: [0-9]*px [0-9]*px;" data-sticker-id=".*"><\/span>//g' newstickers >> neweststickers
 rm stickerlines
