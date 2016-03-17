f8a0eb29582f5d87c2194dfb97fb90f4bb58158e
alexskc <alexskc@autistici.org>
2016-03-17 11:21:07 -0500

    Closes #3

--- main.py
+++ main.py
@@ -31,8 +31,10 @@ while True:
                 filename = update['message']['text']
                 user = update['message']['chat']['id'] 
                 stickertitle = BeautifulSoup(urllib2.urlopen(filename)).title.string
+                name = update['message']['from']['first_name']
+                if 'last_name' in ['message']['from']: name = " ".join((name, update['message']['from']['last_name']))
                 requests.get(url + 'sendMessage', params=dict(chat_id=update['message']['chat']['id'], text="Fetching \"" + stickertitle + "\""))
-                print update['message']['from']['first_name'] + " " + update['message']['from']['last_name']+ " (" + str(user) + ")"+ " requested " + filename
+                print name + " (" + str(user) + ")"+ " requested " + filename
                 subprocess.call("./imagedl.sh " + filename + " " + str(user), shell=True)
             else:
                  requests.get(url + 'sendMessage', params=dict(chat_id=update['message']['chat']['id'], text="That doesn't appear to be a valid URL. To start, send me a URL that starts with \"https://store.line.me/stickershop/product/\""))
